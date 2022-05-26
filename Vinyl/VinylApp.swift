68cfb367f5414d4271990684c0e7766a923c0595
Thomas Gray <tomalexander1999@googlemail.com>
2022-05-26 21:43:46 +0100

    Initial Commit

---
+++ Vinyl/VinylApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylApp.swift
+//  Vinyl
+//
+//  Created by Thomas Gray on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct VinylApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
